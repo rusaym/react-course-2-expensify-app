e50cf0923809a9ee96f95bcfda910543fee89de6
Ruslan Mavletkulov <mavletkulov@rusapr.ru>
2020-12-23 15:52:00 +0300

    commented dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,18 +12,18 @@ import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 100 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 10000 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+// store.dispatch(addExpense({ description: 'Water bill', amount: 100 }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 10000 }));
+// store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
 //store.dispatch(setTextFilter('Gas'));
 
 // setTimeout(() => {
 //     store.dispatch(setTextFilter('bill'));
 // }, 3000);
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
